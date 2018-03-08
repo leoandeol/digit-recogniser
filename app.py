9be9fcea669e58f00c75897e86d2bf5e2f1952f8
Léo Andéol <leo.andeol@gmail.com>
2018-03-08 09:02:03 +0100

    Reorganised the code and improved the DNN

--- app.py
+++ app.py
@@ -10,48 +10,13 @@ import cv2
 import tensorflow as tf
 import scipy
 
+from build import *
+
 # window variables
 
 running = True
 drawing = False
 last_x, last_y = -1, -1
-
-# cleaning
-
-tf.reset_default_graph()
-
-# construction
-
-n_inputs = 28*28
-n_hidden1 = 300
-n_hidden2 = 100
-n_outputs = 10
-
-X = tf.placeholder(tf.float32, shape=[None, n_inputs], name="X")
-y = tf.placeholder(tf.int64, shape=[None], name="y")
-
-with tf.name_scope("dnn"):
-    hidden1 = tf.layers.dense(X, n_hidden1, name="hidden1", activation=tf.nn.relu)
-    hidden2 = tf.layers.dense(hidden1, n_hidden2, name="hidden2", activation=tf.nn.relu)
-    logits = tf.layers.dense(hidden2, n_outputs, name="outputs")
-    
-with tf.name_scope("loss"):
-    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
-    loss = tf.reduce_mean(xentropy, name="loss")
-    
-learning_rate = 0.01
-
-with tf.name_scope("train"):
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    training_op = optimizer.minimize(loss)
-    
-with tf.name_scope("eval"):
-    correct = tf.nn.in_top_k(logits, y, 1)
-    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-    
-init = tf.global_variables_initializer()
-saver = tf.train.Saver()
-
 
 def draw(event, x, y, flags , param):
     global last_x, last_y, drawing, img3
--- train.py
+++ train.py
@@ -3,11 +3,15 @@
 Éditeur de Spyder
 
 Algo génétique todo
+generaliser phase construction
 """
 import time
-import numpy as np
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+
+from build import *
+
+# import data
 
 mnist = input_data.read_data_sets("C:\\Users\\leoan\\Downloads")
 
@@ -16,45 +20,9 @@ X_test = mnist.test.images
 y_train = mnist.train.labels.astype("int")
 y_test = mnist.test.labels.astype("int")
 
-# cleaning
-
-tf.reset_default_graph()
-
-# construction
-
-n_inputs = 28*28
-n_hidden1 = 300
-n_hidden2 = 100
-n_outputs = 10
-
-X = tf.placeholder(tf.float32, shape=[None, n_inputs], name="X")
-y = tf.placeholder(tf.int64, shape=[None], name="y")
-
-with tf.name_scope("dnn"):
-    hidden1 = tf.layers.dense(X, n_hidden1, name="hidden1", activation=tf.nn.relu)
-    hidden2 = tf.layers.dense(hidden1, n_hidden2, name="hidden2", activation=tf.nn.relu)
-    logits = tf.layers.dense(hidden2, n_outputs, name="outputs")
-    
-with tf.name_scope("loss"):
-    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
-    loss = tf.reduce_mean(xentropy, name="loss")
-    
-learning_rate = 0.01
-
-with tf.name_scope("train"):
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    training_op = optimizer.minimize(loss)
-    
-with tf.name_scope("eval"):
-    correct = tf.nn.in_top_k(logits, y, 1)
-    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-    
-init = tf.global_variables_initializer()
-saver = tf.train.Saver()
 
 # execution
-
-n_epochs = 40
+n_epochs = 100
 batch_size = 50
 n_batches_per_epoch = mnist.train.num_examples // batch_size
 timer = time.clock()
@@ -76,18 +44,3 @@ with tf.Session() as sess:
 end = time.clock()
 total = end - beginning
 print("Total duration",total,"s")
-# =============================================================================
-# 
-# cols = tf.contrib.learn.infer_real_valued_columns_from_input(X_train)
-# dnn_clf_tflearn = tf.contrib.learn.DNNClassifier(hidden_units=[300, 100], 
-#                                                  n_classes=10, feature_columns=cols)
-# dnn_clf = tf.contrib.learn.SKCompat(dnn_clf_tflearn)
-# dnn_clf.fit(X_train, y_train, batch_size=50, steps=40000)
-# 
-# from sklearn.metrics import accuracy_score
-# 
-# y_pred = dnn_clf.predict(X_test)
-# 
-# print("\n\n\nResultats : ")
-# print(accuracy_score(y_test, y_pred['classes']))
-# =============================================================================
